2bd7b72c3cf4c5fb18a8fa6d67d5437c7a1b830d
mdn070681 <mdn070681@gmail.com>
2017-09-10 22:15:42 +0300

    parse silpo with pagination

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -80,56 +80,81 @@ class ParseController extends Controller
     {
 
         $url = 'http://silpo.ua/ru/actions/priceoftheweek/';
+        $start = 0;
+        $end = 100;
+
         $html = file_get_contents($url);
         $doc = PhpQuery::newDocument($html);
 
-        $shop = '\images\silpo-small.png';
         $name_action = $doc->find("div.ots div div div font:first")->text();
-
-
-
-
 
         Product::where('name_action', '=', $name_action)->delete(); //тут нужно подумать как не записывать уже записанное
 
-        foreach ($doc->find("div.ots > div.photo ") as $div) {
-            $div = pq($div);
+        function parser($url, $start, $end)
+        {
 
-            $desc = $div->find('h3')->text();
-            $price_dollar = $div->find('.pr span.hrn')->text();
-            $price_cent = $div->find('.pr span.kop')->text();
-            $price = $price_dollar + $price_cent/100;
-            $price_dollar_sale = $div->find('.price_2014_new span.hrn')->text();
-            $price_cent_sale = $div->find('.price_2014_new span.kop')->text();
-            $price_sale = $price_dollar_sale + $price_cent_sale/100;
-            $href_img = $div->find('img')->attr('src');
-            $img = 'http://silpo.ua/' . $href_img;
+            if ($start < $end) {
 
-            if (!empty($price_sale) && !empty($price)) {
-                $sale = ($price - $price_sale) / $price * 100;
-                $sale = ceil($sale);
-            }
+                $html = file_get_contents($url);
+                $doc = PhpQuery::newDocument($html);
 
-            $product = new Product();
+                $shop = '\images\silpo-small.png';
+                $name_action = $doc->find("div.ots div div div font:first")->text();
 
+                foreach ($doc->find("div.ots > div.photo ") as $div) {
+                    $div = pq($div);
+
+                    $desc = $div->find('h3')->text();
+                    $price_dollar = $div->find('.pr span.hrn')->text();
+                    $price_cent = $div->find('.pr span.kop')->text();
+                    if (empty($price_dollar) || empty($price_cent)) {
+                        $price_dollar = 0;
+                        $price_cent = 0;
+                    }
+                    $price = $price_dollar + $price_cent / 100;
+                    $price_dollar_sale = $div->find('.price_2014_new span.hrn')->text();
+                    $price_cent_sale = $div->find('.price_2014_new span.kop')->text();
+                    $price_sale = $price_dollar_sale + $price_cent_sale / 100;
+                    $href_img = $div->find('img')->attr('src');
+                    $img = 'http://silpo.ua/' . $href_img;
+
+                    if (!empty($price_sale) && !empty($price)) {
+                        $sale = ($price - $price_sale) / $price * 100;
+                        $sale = ceil($sale);
+                    }
+
+                    $product = new Product();
 //            $product->name = $name;
-            $product->name_action = $name_action;
-            $product->shop = $shop;
-            $product->img = $img;
-            $product->description = $desc;
-            if (empty($price)) {
-                $price = null;
-                $sale = 0;
+                    $product->name_action = $name_action;
+                    $product->shop = $shop;
+                    $product->img = $img;
+                    $product->description = $desc;
+                    if (empty($price)) {
+                        $price = null;
+                        $sale = 0;
+                    }
+                    $product->price = $price;
+                    $product->price_sale = $price_sale;
+                    $product->sale = $sale;
+                    $product->category_id = 1;
+                    $product->tag_id = 1;
+                    $product->save();
+                }
+
+                $http = 'http://silpo.ua';
+                $next1 = $doc->find('div.page div.act')->next()->find('a')->attr('href');
+                $next = $http . $next1;
+
+                if (!empty($next1)) {
+
+                    $start++;
+                    parser($next, $start, $end);
+                }
             }
-            $product->price = $price;
-            $product->price_sale = $price_sale;
-            $product->sale = $sale;
-            $product->category_id = 1;
-            $product->tag_id = 1;
-            $product->save();
-            
-//            dump($img);
         }
+
+        parser($url, $start, $end);
+
         return redirect()->route('home');
     }
 }

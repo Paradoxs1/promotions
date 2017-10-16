dcc3dd3756fa6cf564e3bd2d7723e14846a0071c
Paradoxs <dimasherstuyk@gmail.com>
2017-10-16 19:25:40 +0300

    phantomjs

--- app/Parser/ShopsParser.php
+++ app/Parser/ShopsParser.php
@@ -10,62 +10,53 @@ class ShopsParser extends Model
     {
         $atb = new AtbParser();
         $atb->parser();
-        return redirect()->route('promotions');
     }
 
     public function SilpoParser()
     {
         $silpo = new SilpoParser();
         $silpo->parser();
-        return redirect()->route('promotions');
     }
 
     public function KlassParser()
     {
         $klass = new KlassParser();
         $klass->parser();
-        return redirect()->route('promotions');
     }
 
     public function PosadParser()
     {
         $posad = new PosadParser();;
         $posad->parser();
-        return redirect()->route('promotions');
     }
 
     public function BrusnichkaParser()
     {
         $brusnichka = new BrusnichkaParser();
         $brusnichka->parser();
-        return redirect()->route('promotions');
     }
 
     public function VelmarketParser()
     {
         $velmarket = new VelmarketParser();
         $velmarket->parser();
-        return redirect()->route('promotions');
     }
 
     public function TavriaParser()
     {
         $tavria = new TavriaParser();
         $tavria->parser();
-        return redirect()->route('promotions');
     }
 
     public function OkwineParser()
     {
         $okwine = new OkwineParser();
         $okwine->parser();
-        return redirect()->route('promotions');
     }
 
     public function AntoshkaParser()
     {
         $antoshka = new AntoshkaParser();
         $antoshka->parser();
-        return redirect()->route('promotions');
     }
 }
--- app/Parser/SilpoParser.php
+++ app/Parser/SilpoParser.php
@@ -10,22 +10,71 @@ class SilpoParser extends Parser
 {
     public function parser()
     {
+
         $url = [
-            'https://silpo.ua/offers/cina-tizhnya',
-            'https://silpo.ua/offers/akciyi-vlasnogo-importu',
-            'https://silpo.ua/offers/garyacha-propoziciya',
-            'https://silpo.ua/offers/tematichni-tizhni',
-            'https://silpo.ua/offers/dityachi-tizhni',
-            'https://silpo.ua/offers/other'
+            'https://silpo.ua/offers',
+            'https://silpo.ua/offers?offset=30',
+            'https://silpo.ua/offers?offset=60',
+            'https://silpo.ua/offers?offset=90',
+            'https://silpo.ua/offers?offset=120'
         ];
-        $start = 0;
-        $end = 100;
 
         foreach ($url as $u) {
-            $this->SilpoShop($u, $start, $end);
-        }
+            $html = shell_exec("phantomjs js/parser.js $u");
+            dd($html);
+            $doc = PhpQuery::newDocument($html);
 
-        return true;
+            foreach ($doc->find(".product-list li.normal") as $div) {
+                $div = pq($div);
+
+                $name = $div->find('.product-list__item-title')->text();
+
+                $img = $div->find('.product-list__item-image img')->attr('href');
+
+                $price_old = $div->find('.product-price__old')->text();
+
+                if (empty($price_old)) {
+                    $price_old = 0;
+                }
+
+                $price = (float)$price_old;
+                $price = number_format($price, 2, '.', '');
+
+                $price_dollar_sale = $div->find('.product-price__integer')->text();
+                $price_cent_sale = $div->find('.product-price__fraction')->text();
+                $price_sale = $price_dollar_sale + $price_cent_sale / 100;
+                $price_sale = (float)$price_sale;
+                $price_sale = number_format($price_sale, 2, '.', '');
+
+                if (!empty($price_sale) && !empty($price) && $price != 0) {
+                    $sale = ($price - $price_sale) / $price * 100;
+                    $sale = ceil($sale);
+                }
+
+                $product = new Product();
+                $product->name_action = 'Акции Сильпо';
+                $product->shop_id = "2";
+                $product->shop = '\images\silpo-small.png';
+                $product->img = $img;
+                $product->name = $name;
+                if (empty($price)) {
+                    $price = null;
+                    $sale = 0;
+                }
+                $product->price = $price;
+                $product->price_sale = $price_sale;
+                $product->sale = $sale;
+
+                $oldProduct = Product::where('img', $img)->first();
+
+                if ($oldProduct) {
+                    $oldProduct->status = 1;
+                    $oldProduct->save();
+                } else {
+                    $product->save();
+                }
+            }
+        }
     }
 
     protected function SilpoShop($u, $start, $end)
